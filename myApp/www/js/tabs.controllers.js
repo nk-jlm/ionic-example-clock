397be3579d61e0b0ee9c22504c9279a3b08f639c
Nina <nina.koval92@gmail.com>
2016-02-21 20:03:44 +0200

    update(app): add clock controller

--- myApp/www/js/tabs.controllers.js
+++ myApp/www/js/tabs.controllers.js
@@ -19,10 +19,4 @@ angular.module('starter.controllers', [])
 
 .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
   $scope.chat = Chats.get($stateParams.chatId);
-})
-
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
 });
